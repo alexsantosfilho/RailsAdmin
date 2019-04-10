9b0b3e15b1587c61249260ffc0ac3f7ba23cb04c
alexsantosfilho <devalexsandro@gmail.com>
2019-04-10 14:46:39 -0300

    New test bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     config.parent_controller = 'ApplicationController'
+    rescue_from CanCan::AccessDenied do |exception|
+        Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
+        # ...
+    end
 end
