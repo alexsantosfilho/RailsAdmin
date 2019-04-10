f49f94bc14f872bfcda60d13073b61e04584ac7a
alexsantosfilho <devalexsandro@gmail.com>
2019-04-10 13:51:29 -0300

    remove destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
     enum status: [:active, :inactive]
-    has_many :product_quantities, :dependent => :destroy
+    has_many :product_quantities
 
     mount_uploader :photo, PhotoUploader
 end
