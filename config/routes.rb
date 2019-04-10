f3245fa13616ae2ff3505489a01e723956c35f1b
alexsantosfilho <devalexsandro@gmail.com>
2019-04-10 11:38:53 -0300

    test route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount RailsAdmin::Engine => '/', as: 'rails_admin'
+  mount RailsAdmin::Engine => 'railsadmin.herokuapp.com/', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
