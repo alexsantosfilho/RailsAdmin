42ecc707c16edb8641d08cbcd47ad2f3bc6f8c87
alexsantosfilho <devalexsandro@gmail.com>
2019-04-12 17:06:57 -0300

    Eroo

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    # etc
-  }
+  devise_for :users
   mount RailsAdmin::Engine => '/', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
