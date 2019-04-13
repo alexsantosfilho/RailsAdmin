6ec2145f5b7a4f1b2e6333eb71802a36f41bffab
Alexsandro Santos <devalexsandro@gmail.com>
2019-04-12 23:20:35 -0300

    r

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,12 +8,11 @@ module Users
           set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
         else
           session['devise.facebook_data'] = request.env['omniauth.auth']
-          redirect_to new_user_registration_url
+
         end
       end
 
       def failure
-        redirect_to new_user_session_path
       end
     end
   end
