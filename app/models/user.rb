d37e3b7c7fa93736b263c20fe51207e458f8c29e
alexsantosfilho <devalexsandro@gmail.com>
2019-04-12 23:48:30 -0300

    up devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   include Omniauthenticable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
-    enum kind: [:salesman, :manager]
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable, omniauth_providers: %i[facebook]
+
+  enum kind: [:salesman, :manager]
     enum status: [:active, :inactive]
     has_many :comissions
     has_many :addresses
