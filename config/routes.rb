a4acc885508624db2ea7594328c164a76480b475
alexsantosfilho <devalexsandro@gmail.com>
2019-04-16 11:10:23 -0300

    Fix layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "http://localhost:3000"
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     # etc
